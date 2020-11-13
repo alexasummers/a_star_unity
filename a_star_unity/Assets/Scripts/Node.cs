a2c063fc227b69474cd19ddde8c75857fdc53144
Alexa <alexas0202@gmail.com>
2020-11-13 12:06:19 -0600

    Created Scripts & moved to respective folders

---
+++ a_star_unity/Assets/Scripts/Node.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public bool obstacle;
+    public Vector3 worldPosition;
+    
+    public Node(bool _obstacle, Vector3 _worldPosition) 
+    {
+        obstacle = _obstacle;
+        worldPosition = _worldPosition;
+    }
+}
