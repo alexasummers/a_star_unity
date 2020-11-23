f6176b4e43f45691ebe7c39b9e48bfdb9ea1c86c
Alexa Summers <alexas0202@gmail.com>
2020-11-23 11:20:13 -0600

    Added Scripts files

---
+++ a_star_unity/Scripts/Pathfinding.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic; //using a list
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour { //must extend monobehavior because we are using some of the funcitonality
+
+
+	void Awake() { //getting the grid
+    
+	}
+
+	void Update() { //updates the path drawn between point A and point B as we move the points
+
+	}
+
+	void FindPath(Vector3 startPos, Vector3 targetPos) { //generates the path between point A and point B
+		
+	}
+
+	void RetracePath(Node startNode, Node endNode) { //traces the path between point A and point B
+		
+	}
+
+	int GetDistance(Node nodeA, Node nodeB) { //get the distance between two given nodes
+		
+    }
+
+}
